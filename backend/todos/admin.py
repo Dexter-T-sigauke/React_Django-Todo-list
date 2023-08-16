ff64ccda76a29804d24552096452a24d0a8a15f1
Dexter T Sigauke <tafadzwasigauke123@gmail.com>
2023-08-16 15:28:42 +0200

    react and django simple todo

---
+++ backend/todos/admin.py
@@ -0,0 +1,6 @@
+# todos/admin.py
+from django.contrib import admin
+
+from .models import Todo
+
+admin.site.register(Todo)
---
+++ backend/todos/views.py
@@ -0,0 +1,13 @@
+# todos/views.py
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
